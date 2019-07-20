840828decb68613f404d3f139d452861ae803392
Pablo Lacasta <bapalnas@gmail.com>
2019-07-20 20:08:10 +0200

    dm

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 document.querySelector('.dropdown').addEventListener('click', event => {
-    event.target.style.visibility = 'hidden';
+    event.target.style.display = 'block';
+    document.querySelector('.dropdownMenu').style.display = 'block';
+    // document.querySelector('.enlace').style.display ="block";
+    })
+    document.querySelector('.dropdownMenu').addEventListener('click', event =>{
+        // document.querySelector('.enlace').style.display = "none";
+
+        event.target.parentNode.style.display = "none";
+        // event.target.parentNode.parentNode.style.display = "none";
+        document.querySelector('.dropdown').style.display ="block";
+
+    })
+
+    document.querySelector('.enlace').addEventListener('click', event =>{
+        // document.querySelector('.enlace').style.display = "none";
+
+        event.target.parentNode.parentNode.style.display = "none";
+        // event.target.parentNode.parentNode.style.display = "none";
+        document.querySelector('.dropdown').style.display ="block";
+
     })
