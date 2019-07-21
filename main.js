2b408b20c808cc4471c79bb7daf588ab6b97d825
Pablo Lacasta <bapalnas@gmail.com>
2019-07-21 10:56:18 +0200

    dm casi terminado

--- main.js
+++ main.js
@@ -9,5 +9,31 @@ document.querySelector('.dropdown').addEventListener('click', event => {
     })
     
     document.querySelector('.enlaceContainer').addEventListener('click', event => {
-        event.target.childNodes.click();
+        event.target.getElementById('.enlace').click();
     })
+
+    $(window).scroll(function(){
+        var bar = $("div.bar");
+        
+        var fixed_position = $("div.bar").offset().top;
+        var fixed_height = $("div.bar").height();
+        
+        var checkpos = function(id){
+          var toCross_position = $("#" + id + ".dropdownBlack").offset().top;
+          var toCross_height = $("#" + id + ".dropdownBlack").height();
+          
+          var fixedtgt = $("div.bar[data-id='"+ id +"']");
+          
+          if (fixed_position + fixed_height  < toCross_position) {
+            fixedtgt.css({'background-color':'#F2F1F0','z-index':'0' });
+          } else if (fixed_position > toCross_position + toCross_height) {
+            fixedtgt.css({'background-color':'#F2F1F0','z-index':'0' });
+          } else {
+            fixedtgt.css({'background-color':' #1C1B18','z-index':'1' });
+          }
+        }
+        
+        checkpos("divone");
+        checkpos("divtwo");
+        checkpos("divthree");
+      });
